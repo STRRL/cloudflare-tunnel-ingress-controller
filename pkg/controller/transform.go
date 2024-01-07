0a1ca52ddaa19d0d7813d9e1c540633d1c9feaaf
Erik Rasmussen <ecr.referee@hotmail.com>
2024-01-07 15:04:37 -0600

    Update service status when reconciling

--- pkg/controller/transform.go
+++ pkg/controller/transform.go
@@ -32,7 +32,6 @@ func FromIngressToExposure(ctx context.Context, logger logr.Logger, kubeClient c
 		}
 
 		hostname := rule.Host
-
 		scheme := "http"
 
 		if backendProtocol, ok := getAnnotation(ingress.Annotations, AnnotationBackendProtocol); ok {
@@ -96,15 +95,26 @@ func FromIngressToExposure(ctx context.Context, logger logr.Logger, kubeClient c
 				return nil, errors.Errorf("path type in ingress %s/%s is %s, which is not supported", ingress.GetNamespace(), ingress.GetName(), *path.PathType)
 			}
 
-			pathPrefix := path.Path
-
 			result = append(result, exposure.Exposure{
 				Hostname:              hostname,
 				ServiceTarget:         fmt.Sprintf("%s://%s:%d", scheme, host, port),
-				PathPrefix:            pathPrefix,
+				PathPrefix:            path.Path,
 				IsDeleted:             isDeleted,
 				ProxySSLVerifyEnabled: proxySSLVerifyEnabled,
 			})
+
+			if service.Status.LoadBalancer.Ingress == nil {
+				service.Status.LoadBalancer.Ingress = []v1.LoadBalancerIngress{{Hostname: hostname}}
+			} else {
+				service.Status.LoadBalancer.Ingress = append(service.Status.LoadBalancer.Ingress, v1.LoadBalancerIngress{
+					Hostname: hostname,
+				})
+			}
+
+			err = kubeClient.Status().Update(ctx, &service)
+			if err != nil {
+				return nil, errors.Wrapf(err, "update service %s", service.Name)
+			}
 		}
 	}
 
